a76749457297ec9375276eb36b826e86ce6c0758
Mauricio Gemelli Vigolo <mauriciogv@sanepar.com.br>
2017-03-22 14:10:14 -0300

    new: mvn processor impl

--- bin/nilegacy.js
+++ bin/nilegacy.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
+var MvnProcessor = require('../lib/processors/mvn.processor');
+
+var processor = new MvnProcessor();
+processor.isConfigured();
+
 console.log(process.argv.slice(2));
